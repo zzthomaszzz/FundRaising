427c4ec4f9ef10dfe365aad28dab9e4169cfa566
zzthomaszzz <60630447+zzthomaszzz@users.noreply.github.com>
2020-05-16 11:32:37 +1200

    Workingvariable costs and how many items to be sold

--- main_routine/main_routine.py
+++ main_routine/main_routine.py
@@ -2,12 +2,12 @@
 from function.function_01 import not_blank
 # Main routine goes here
 name = not_blank("This response can not be blank or contain number", "What is your name? ", True)
-print(name)
+print("Please enter items that you would like to sell, if you would like to stop, please enter 'xxx'T")
 
-items = []
+items = {}
 loop = True
 while loop:
-    response = not_blank("this cant be blank", "Items for selling? ", False)
+    response = not_blank("this cant be blank", "Items: ", False)
     if response == 'xxx':
         if len(items) > 0:
             break
@@ -15,22 +15,44 @@ while loop:
             print("Please add at least one item. ")
             continue
     else:
-        items.append(response)
-print("These are your items")
-for item in items:
+        items[response] = 'none'
+print("Please enter the quantity for items that you have chosen to sell")
+for i in items:
     loop = True
     while loop:
-        quantity = input("How many of the".format(item))
-        if quantity < 0:
-            print("The number can't be less than 0")
-            continue
-        elif quantity.isdecimal():
-            print("The number can't be a decimal")
-            continue
-        else:
-            items.append(quantity)
+        number = input("Quantity for " + i + ": ")
+        if number.isdigit():
+            items[i] = number
             break
-print(items)
+        else:
+            print("Please enter a number value. ")
+            continue
+sum = 0
+print("Please enter the cost for each item (in dollar)")
+for i in items:
+    loop = True
+    while loop:
+        cost = input("what is the cost for " + i + " ? ")
+        try:
+            cost = float(cost)
+            cost_2 = items.get(i)
+            cost_2 = float(cost_2)
+            cost_1 = cost * cost_2
+            sum += cost_1
+            break
+        except ValueError:
+            print("Please enter a value in dollar.")
+print("here is the sub-total cost for your items")
+print("Sub-total cost: " + sum)
+
+
+
+
+
+
+
+
+
 
 
 
